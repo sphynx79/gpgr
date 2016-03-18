bc1c5a0fc5999c2e50df0f7bb8b23a69bd94445f
sphynx79 <miboscol@gmail.com>
2016-03-18 11:31:10 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,57 @@
+
+# Gpgr: Criptare e descriptare dati con gpg in Ruby
+&nbsp;
+
+[TOC]
+
+&nbsp;
+
+### Introduzione
+Applicazione scritta in ruby per crittografia di dati attraverso **gnupg**.
+&nbsp;
+
+### Descrizione
+Ho creato questa applicazione perchè aveva la necessità di criptare file importanti senza lasciare tracce nella macchina in cui viene eseguita, per un ottimo livello di crittografia mi sono affidato a [Gnupg](https://www.gnupg.org/), per quantyo riguarda l'eliminazione dei file in modo permanente mi appoggio all'utility  [Eraser](http://eraser.heidi.ie/).
+Per i passaggi intermedi utilizzo l'utility [7-zip](www.7-zip.org/).
+&nbsp;
+
+
+### Utilizzo
+L'ulizzo è molto semplice l'applicativo funziona da linea di comando, per l'help:
+```
+ruby gpg.rb -h
+
+Usage: gpgr2.exe -e -p file -m mode
+Options
+    -d, --decrypt                    Start in decrypt mode
+    -e, --encrypt                    Start in encrypt mode
+    -p, --path PATH                  Path della directory o del file da utilizzare
+    -m, --method METHOD              Algoritmo usato da eraser per eliminare i file
+    -h, --help                       Display this screen
+```
+Di defaul per l'elimanzione dei file viene usato il metodo Gutmann(35 pass), è possibile selezionare tra i metodi disponibili (Gutmann|DoD|DoD_E|First_Last2k|Schneier|Random|Library) quello desiderato passandolo come argomento al parametr \--method.
+Per maggiori informazioni sui metodi disponibili vedere la documentazione di Eraser.
+
+##### Criptare
+```
+ruby gpg.rb -e -p "YOUR_PATH_DATA"
+```
+E' possibile specificare il metodo di eliminazione:
+```
+ruby gpg.rb -e -m Schneier -p "YOUR_PATH_DATA"
+```
+
+##### Decriptare
+```
+ruby gpg.rb -d -p "YOUR_CRIPT_FILE"
+```
+E' possibile specificare il metodo di eliminazione:
+```
+ruby gpg.rb -d -m Schneier -p "YOUR_CRIPT_FILE"
+```
+&nbsp;
+
+
+### Dimostrazione
+Qui di seguido un video dimostrativo della nostra applicazione:
+@[](https://www.youtube.com/watch?v=0DmhFTv3nbk)
---
+++ gpg.rb
@@ -0,0 +1,188 @@
+require 'optparse'
+require 'open3'
+
+
+ZIP     =  "\"7-Zip\\7z.exe\"".freeze
+GPG     =  "\"GPG\\gpg2.exe\"".freeze
+ERASER  =  "\"eraser\\Eraserl.exe\"".freeze
+
+class Gpg
+
+   attr_accessor :operation, :method, :path, :nome_file, :file, :gpg_file
+   def initialize(data = {})
+      @operation        = if data[:encrypt] then "encrypt" else "decrypt" end
+      @method           = if data[:method].nil? then "Gutmann" else data[:method] end
+      @path             = data[:path]
+      @nome_file        = path.split('\\')[-1]
+      @file             = if not @nome_file.include? "." then  path.gsub(nome_file, "#{nome_file}.zip") else path end
+      @gpg_file         = path.gsub(nome_file, "#{nome_file}.gpg")
+   end 
+
+   def start
+      if @operation == "encrypt"
+         start_encrypt
+      else
+         start_decrypt
+      end
+   end
+
+   def start_encrypt
+      if directory?
+         file_zip = crea_archivio
+         encrypt
+         elimina_file "folder", path
+         elimina_file "file"  , file_zip
+         sleep 1
+      else
+         encrypt
+         elimina_file "file", path
+      end
+   end
+
+   def start_decrypt
+      decrypt_file = decrypt
+      if decrypt_file.include? ".zip"
+         estrai_archivio decrypt_file
+         elimina_file "file", decrypt_file
+      end
+      elimina_file "file", path
+   end
+
+   def estrai_archivio decrypt_file
+      Open3.popen3("#{ZIP}  x -y  \"#{decrypt_file}\"") do |stdin, stdout, stderr, wait_thr|
+         stdout.each_line { |line| puts line}
+      end
+   end
+
+   def decrypt
+      decrypt_file = path.gsub(".gpg","")
+      if not decrypt_file.include? "."
+         decrypt_file = decrypt_file+".zip"
+      end
+      Open3.popen3( "#{GPG} --output \"#{decrypt_file}\" -d \"#{path}\"") do |stdin, stdout, stderr, wait_thr|
+         stdout.each_line { |line| puts line }
+      end
+      return decrypt_file
+   end
+
+   def crea_archivio
+      zip = path.gsub(nome_file, "#{nome_file}.zip")
+      Open3.popen3("#{ZIP}  a -tzip \"#{zip}\" \"#{path}\\\" -mx0") do |stdin, stdout, stderr, wait_thr|
+         stdout.each_line { |line| puts line }
+      end
+      return zip
+   end
+
+   def encrypt
+      file     = if not @nome_file.include? "." then  path.gsub(nome_file, "#{nome_file}.zip") else path end
+      gpg_file = path.gsub(nome_file, "#{nome_file}.gpg")
+      Open3.popen3( "#{GPG} --output \"#{gpg_file}\" -e -r \"sphynx browserino\" \"#{file}\"") do |stdin, stdout, stderr, wait_thr|
+         stdout.each_line { |line| puts line }
+         #pid = wait_thr.pid
+         #exit_status = wait_thr.value
+      end
+   end
+
+   def elimina_file type, file_da_eliminare
+      Open3.popen3( "#{ERASER}  -method #{method} -queue -resultsonerror -#{type} \"#{file_da_eliminare}\"") do |stdin, stdout, stderr, wait_thr|
+         stdout.each_line { |line| puts line }
+         #pid = wait_thr.pid
+         #exit_status = wait_thr.value
+      end
+   end
+
+   def directory?
+      if nome_file.include? "."
+         return false
+      else
+         return true
+      end
+   end
+
+end
+
+
+
+options = {}
+
+optparse = OptionParser.new do |opts|
+
+   opts.banner = "Usage: gpgr2.exe -e -p file -m mode"
+   opts.separator  "Options"
+
+
+   opts.on('-d', '--decrypt', 'Start in decrypt mode') do |decrypt|
+      options[:decrypt] = true
+   end
+
+   opts.on('-e', '--encrypt', 'Start in encrypt mode') do |encrypt|
+      options[:encrypt] = true
+   end
+
+   opts.on('-p', '--path PATH ', 'Path della directory o del file da utilizzare') do |path|
+      options[:path] = path
+   end
+
+   opts.on('-m', '--method METHOD', 'Algoritmo usato da eraser per eliminare i file') do |method|
+      options[:method] = method
+   end
+
+   opts.on('-h', '--help', 'Display this screen') do
+      puts opts
+      exit
+   end
+end
+
+
+
+begin
+   optparse.parse!
+
+   data = [:decrypt, :encrypt]
+   missing = data.select{|param| options[param].nil?}
+   if missing.empty?                                            
+      puts "Selezionare parametro -e oppure -d non possono essere selezionato contemporaneamente"                                         
+      puts optparse                                                  
+      exit                                                           
+   end 
+   if missing.size == 2                                          
+      puts "Selezionare -e oppure -d, senza questi non posso continuare"                                         
+      puts optparse                                                  
+      exit                                                           
+   end 
+
+   data = [:path]
+   missing = data.select{|param| options[param].nil?}
+   if not missing.empty?                                            
+      puts "Deve essere specificato almeno un path: -p PATH"                                         
+      puts optparse                                                  
+      exit                                                           
+   end 
+
+   data = [:method]
+   metodi = ["Gutmann" , "DoD" , "DoD_E" , "First_Last2k" , "Schneier" , "Random"  , "Library"]
+
+   if not metodi.include? options[:method]  && if not options[:method].nil?
+      puts "Metodo inserito per eraser non corretto deve selezionare uno di quest algoritmi:"
+      puts "Gutmann | DoD | DoD_E | First_Last2k | Schneier | Random  | Library"
+      puts optparse                                                  
+      exit         
+   end
+end
+missing = data.select{|param| options[param].nil?}
+if not missing.empty?                                            
+   puts "Metodo non selezionato verra usato di default Schneier(35 pass)"                                                                                                                                                  
+end 
+
+rescue OptionParser::InvalidOption, OptionParser::MissingArgument      
+   # Con $! stampo ERROR_INFO 
+   # vedere http://blog.honeybadger.io/working-with-ruby-exceptions-outside-of-the-rescue-block/
+   puts $!.to_s                                                           
+   puts optparse                                                          
+   exit                                                                   
+end 
+
+gpg = Gpg.new(options)
+
+gpg.start
+
